2930d5f51ab6d0e975a64415465acf7c5af7615c
tph007 <tim@hathaway.net>
2019-03-18 14:28:03 -0700

    Add time to returned forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,5 @@
 const request = require('request')
-
+const moment = require('moment')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/b99a0098be9586a2e3853323eeedb71b/' + latitude + ',' + longitude
 
@@ -9,7 +9,12 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            var timenow = moment(body.currently.time*1000).format("MMM Do, YYYY - HH:mm:ss ZZ");
+            weatherData = 'On ' + timenow + ', ' + body.daily.data[0].summary + 
+            ' It is currently ' + body.currently.temperature + 'º.' + 
+            ' There is a ' + body.currently.precipProbability + '% chance of rain.'
+                    
+            callback(undefined, weatherData)
         }
     })
 }
